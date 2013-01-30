61772a559e85d08245f2bdc0d11d0ecb03f711f6
Mark Rada <mrada@marketcircle.com>
2013-01-29 22:41:41 -0500

    Add documentation for various constants for MRI

--- lib/ax_elements/mri.rb
+++ lib/ax_elements/mri.rb
@@ -20,23 +20,37 @@ unless on_macruby?
   KAXStringForRangeParameterizedAttribute = 'AXStringForRange'
 
   unless defined? NSString
+    # Alias for the `String` class, compatible with MacRuby
+    # @return [Class]
     NSString = String
   end
 
   unless defined? NSDictionary
+    # Alias for the `Hash` class, compatible with MacRuby
+    # @return [Class]
     NSDictionary = Hash
   end
 
   unless defined? NSArray
+    # Alias for the `Array` class, compatible with MacRuby
+    # @return [Class]
     NSArray = Array
   end
 
   unless defined? NSDate
+    # Alias for the `Time` class, compatible with MacRuby
+    # @return [Class]
     NSDate = Time
   end
 
+  ##
+  # AXElements extensions to the Symbol class
   class Symbol
 
+    ##
+    # Equivalent to `String#chomp`, coerces receiver to string first
+    #
+    # @return [String]
     def chomp suffix
       to_s.chomp suffix
     end
