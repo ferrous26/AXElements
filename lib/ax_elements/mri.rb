3eb47782ccb4379034ef8c2c31f356da924bb2ed
Mark Rada <mrada@marketcircle.com>
2013-01-30 20:19:08 -0500

    Add type documentation for the KAX constants on MRI

--- lib/ax_elements/mri.rb
+++ lib/ax_elements/mri.rb
@@ -1,24 +1,44 @@
 unless on_macruby?
 
+  # @return [String]
   KAXChildrenAttribute         = 'AXChildren'
+  # @return [String]
   KAXRoleAttribute             = 'AXRole'
+  # @return [String]
   KAXSubroleAttribute          = 'AXSubrole'
+  # @return [String]
   KAXIdentifierAttribute       = 'AXIdentifier'
+  # @return [String]
   KAXWindowCreatedNotification = 'AXWindowCreated'
+  # @return [String]
   KAXMainWindowAttribute       = 'AXMainWindow'
+  # @return [String]
   KAXFocusedAttribute          = 'AXFocused'
+  # @return [String]
   KAXValueChangedNotification  = 'AXValueChanged'
+  # @return [String]
   KAXTitleAttribute            = 'AXTitle'
+  # @return [String]
   KAXURLAttribute              = 'AXURL'
+  # @return [String]
   KAXTitleUIElementAttribute   = 'AXTitleUIElement'
+  # @return [String]
   KAXPlaceholderValueAttribute = 'AXPlaceholderValue'
+  # @return [String]
   KAXWindowRole                = 'AXWindow'
+  # @return [String]
   KAXCloseButtonSubrole        = 'AXCloseButton'
+  # @return [String]
   KAXTrashDockItemSubrole      = 'AXTrashDockItem'
+  # @return [String]
   KAXPosition                  = 'AXPosition'
+  # @return [String]
   KAXSize                      = 'AXSize'
+  # @return [String]
   KAXRTFForRangeParameterizedAttribute    = 'AXRTFForRange'
+  # @return [String]
   KAXIsApplicationRunningAttribute        = 'AXIsApplicationRunning'
+  # @return [String]
   KAXStringForRangeParameterizedAttribute = 'AXStringForRange'
 
   unless defined? NSString
